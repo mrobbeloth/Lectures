39975c4407638ddab35e4bcb21fa8dfde68d0b12
Michael Robbeloth <michael@robbeloth.com>
2026-05-14 16:03:35 -0400

    Add Collataz Sequence as Recursion Example. Changes to support JDK 26

--- Recursion/src/Collatz.java
+++ Recursion/src/Collatz.java
@@ -1,39 +1,35 @@
-import java.util.Scanner;
+void main() {
+    Scanner kb = new Scanner(System.in);
+    IO.print("Starting number: ");
+    int start = Integer.parseInt(kb.nextLine());
+    showSequence(start);
+    IO.println("(" + steps(start) + " steps)");
+}
 
-public class Collatz {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        System.out.print("Starting number: ");
-        int start = Integer.parseInt(kb.nextLine());
-        showSequence(start);
-        System.out.println("("+steps(start)+" steps)");
+private static void showSequence(int n) {
+    IO.print(n + " ");
+    if (n == 1) {
+        return;
     }
-
-    private static void showSequence(int n) {
-        System.out.print(n + " ");
-        if (n == 1) {
-            return;
-        }
-        if (n % 2 == 0) {
-            showSequence(n / 2);
-        } else {
-            showSequence(n * 3 + 1);
-        }
+    if (n % 2 == 0) {
+        showSequence(n / 2);
+    } else {
+        showSequence(n * 3 + 1);
     }
+}
 
-    /**
-     * Count steps to reach 1 in Collatz sequence
-     * @param n Starting number
-     * @return Number of steps to reach 1
-     */
-    public static int steps(int n) {
-        if (n == 1) {
-            return 0;
-        }
-        if (n % 2 == 0) {
-            return 1 + steps(n / 2);
-        } else {
-            return 1 + steps(n * 3 + 1);
-        }
+/**
+ * Count steps to reach 1 in Collatz sequence
+ * @param n Starting number
+ * @return Number of steps to reach 1
+ */
+public static int steps(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    if (n % 2 == 0) {
+        return 1 + steps(n / 2);
+    } else {
+        return 1 + steps(n * 3 + 1);
     }
 }
